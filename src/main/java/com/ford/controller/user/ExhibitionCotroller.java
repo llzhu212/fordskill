d75ebcd12cb1e636c7bfb9468a5a442fd23e0f12
kangyang <kangyang@travelsky.com>
2018-07-06 11:20:00 +0800

    提交

--- src/main/java/com/ford/controller/user/ExhibitionCotroller.java
+++ src/main/java/com/ford/controller/user/ExhibitionCotroller.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ford.controller.user.comment.ExampleComment;
 import com.ford.controller.user.comment.ExhibitionComment;
 import com.ford.entity.exhibition.FordExhibitionSupplier;
 import com.ford.entity.exhibition.FordExhibitionSupplierExample;
@@ -38,24 +39,18 @@ public class ExhibitionCotroller {
 
 	@Autowired
 	private IFordAgentinfoService fordagentinfoService;
-	
+
 	@RequestMapping(value = "/addExhibition.act")
-	 public ModelAndView addExhibition(FordExhibitionUserinfo
-	 fordExhibitionUserinfo) {
+	public ModelAndView addExhibition(FordExhibitionUserinfo fordExhibitionUserinfo) {
 		ModelAndView modelAndView = new ModelAndView();
-//	public ModelAndView addExhibition(@RequestBody String json) {
-//		 fordExhibitionUserinfo = new FordExhibitionUserinfo();
-//		try {
-//			ObjectMapper om = new ObjectMapper();
-////			fordExhibitionUserinfo = om.readValue(json, FordExhibitionUserinfo.class);
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-		if (null==fordExhibitionUserinfo.getCode()||"".equals(fordExhibitionUserinfo.getCode())||null==fordExhibitionUserinfo.getName()||"".equals(fordExhibitionUserinfo.getName())||null==fordExhibitionUserinfo.getPhone()||"".equals(fordExhibitionUserinfo.getPhone())) {
+		boolean flag = false;
+		// 判断时间是否过期
+		if (null == fordExhibitionUserinfo.getCode() || "".equals(fordExhibitionUserinfo.getCode())
+				|| null == fordExhibitionUserinfo.getName() || "".equals(fordExhibitionUserinfo.getName())
+				|| null == fordExhibitionUserinfo.getPhone() || "".equals(fordExhibitionUserinfo.getPhone())) {
 			modelAndView.setViewName("forward:/fordexhibition/forwardAdd.act");
 			return modelAndView;
 		}
-		boolean flag = false;
 		try {
 			fordExhibitionUserinfo.setIswin("0");
 			String time = DateUtil.getCurrentTiem();
@@ -71,13 +66,20 @@ public class ExhibitionCotroller {
 			} catch (Exception e2) {
 				log.error(e2.getMessage());
 			}
-			modelAndView.addObject("chongfuerror", 1).addObject("name", fordExhibitionUserinfo.getName()).addObject("phone", fordExhibitionUserinfo.getPhone()).addObject("code", fordExhibitionUserinfo.getCode());
+			modelAndView.addObject("chongfuerror", 1).addObject("name", fordExhibitionUserinfo.getName())
+					.addObject("phone", fordExhibitionUserinfo.getPhone())
+					.addObject("code", fordExhibitionUserinfo.getCode());
 			modelAndView.setViewName("/user/bm");
+			return modelAndView;
+		}
+		if (DateUtil.compareToday(ExhibitionComment.EXH_ENDDATE)) {
+			modelAndView.addObject("winflag", 0);
+			modelAndView.setViewName("/user/win");
 			return modelAndView;
 		}
 		// 抽奖
 		int i = (int) (Math.random() * 100 + 1);
-		log.error("电话号码:"+fordExhibitionUserinfo.getPhone()+"======抽奖号码；"+i);
+		log.error("电话号码:" + fordExhibitionUserinfo.getPhone() + "======抽奖号码；" + i);
 		try {
 			if (i <= 20) {
 				Map<String, Object> map = new HashMap<>();
@@ -123,35 +125,36 @@ public class ExhibitionCotroller {
 		return modelAndView;
 	}
 
-//	@RequestMapping(value = "/forwardAdd.act")
-//	public void forwardAdd() throws Exception {
-//		ModelAndView modelAndView = new ModelAndView();
-//		try {
-//			DataInputStream in = new DataInputStream(new FileInputStream(new File("E:/私人文件/二手车代码交接/经销商信息表测试用.csv")));
-//			BufferedReader br= new BufferedReader(new InputStreamReader(in,"GBK"));
-//			List<FordAgentinfo> list = new ArrayList<>();
-//			 String line = null;  
-//			 int i = 1;
-//	            while((line=br.readLine())!=null){  
-//	                String item[] = line.split(",");//CSV格式文件为逗号分隔符文件，这里根据逗号切分 
-//	                FordAgentinfo fordAgentinfo = new FordAgentinfo();
-//	                fordAgentinfo.setRegion(item[0]);
-//	                fordAgentinfo.setProvince(item[1]);
-//	                fordAgentinfo.setCity(item[2]);
-//	                fordAgentinfo.setCode(item[3]);
-//	                fordAgentinfo.setAbbreviation(item[4]);
-//	                fordAgentinfo.setName(item[5]);
-//	                fordagentinfoService.addInfo(fordAgentinfo);
-//	                System.out.println("添加第"+i+"条信息");
-//	                list.add(fordAgentinfo);
-//	                i++;
-//	            } 
-//	            System.out.println("读取完毕,总共读取:"+i+"条信息");
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-////		return modelAndView;
-//	}
+	// @RequestMapping(value = "/forwardAdd.act")
+	// public void forwardAdd() throws Exception {
+	// ModelAndView modelAndView = new ModelAndView();
+	// try {
+	// DataInputStream in = new DataInputStream(new FileInputStream(new
+	// File("E:/私人文件/二手车代码交接/经销商信息表测试用.csv")));
+	// BufferedReader br= new BufferedReader(new InputStreamReader(in,"GBK"));
+	// List<FordAgentinfo> list = new ArrayList<>();
+	// String line = null;
+	// int i = 1;
+	// while((line=br.readLine())!=null){
+	// String item[] = line.split(",");//CSV格式文件为逗号分隔符文件，这里根据逗号切分
+	// FordAgentinfo fordAgentinfo = new FordAgentinfo();
+	// fordAgentinfo.setRegion(item[0]);
+	// fordAgentinfo.setProvince(item[1]);
+	// fordAgentinfo.setCity(item[2]);
+	// fordAgentinfo.setCode(item[3]);
+	// fordAgentinfo.setAbbreviation(item[4]);
+	// fordAgentinfo.setName(item[5]);
+	// fordagentinfoService.addInfo(fordAgentinfo);
+	// System.out.println("添加第"+i+"条信息");
+	// list.add(fordAgentinfo);
+	// i++;
+	// }
+	// System.out.println("读取完毕,总共读取:"+i+"条信息");
+	// } catch (Exception e) {
+	// e.printStackTrace();
+	// }
+	//// return modelAndView;
+	// }
 
 	@RequestMapping(value = "/forwardAdd.act")
 	public ModelAndView forwardAdd() throws Exception {
