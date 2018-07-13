9728ffcbb9e1ef3a8da754de8e5c6dc79668becc
kangyang <kangyang@travelsky.com>
2018-07-13 10:55:38 +0800

    提交

--- src/main/java/com/ford/controller/user/comment/ExampleComment.java
+++ src/main/java/com/ford/controller/user/comment/ExampleComment.java
@@ -5,7 +5,7 @@ public class ExampleComment {
 	
 	public static final String EXAM_START_TIME = "2018-07-05 11:00:00";
 	
-	public static final String EXAM_END_TIME = "2018-07-11 12:00:00";
+	public static final String EXAM_END_TIME = "2018-08-11 12:00:00";
 	
 	public static final String EXAM_EXAMADDRESS = "https://ks.wjx.top/jq/25253413.aspx?sojumpparm=";
 	
