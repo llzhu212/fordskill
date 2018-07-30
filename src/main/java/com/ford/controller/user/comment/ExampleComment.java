584c456c8d06a10fcc0d9330fb3571ad415b1456
llzhu <llzhu@travelsky.com>
2018-07-30 09:42:12 +0800

    限制时间

--- src/main/java/com/ford/controller/user/comment/ExampleComment.java
+++ src/main/java/com/ford/controller/user/comment/ExampleComment.java
@@ -3,11 +3,11 @@ package com.ford.controller.user.comment;
 
 public class ExampleComment {
 	
-	public static final String EXAM_START_TIME = "2018-07-05 11:00:00";
+	public static final String EXAM_START_TIME = "2018-08-23 10:30:00";
 	
-	public static final String EXAM_END_TIME = "2018-08-11 12:00:00";
+	public static final String EXAM_END_TIME = "2018-08-23 11:10:00";
 	
 	public static final String EXAM_EXAMADDRESS = "https://ks.wjx.top/jq/26433046.aspx?sojumpparm=";
 	
-	public static final String EXAM_SHOWRUWEI_TIME = "2018-07-01 00:00:00";
+	public static final String EXAM_SHOWRUWEI_TIME = "2018-08-31 00:00:00";
 }
