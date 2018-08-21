7916ab2b2e78c416993695cf596cb1a62e8abe2a
llzhu <llzhu@travelsky.com>
2018-08-21 15:12:37 +0800

    tijiao

--- src/main/java/com/ford/controller/user/comment/ExampleComment.java
+++ src/main/java/com/ford/controller/user/comment/ExampleComment.java
@@ -11,5 +11,5 @@ public class ExampleComment {
 	
 	public static final String EXAM_SHOWRUWEI_TIME = "2018-08-31 00:00:00";
 	
-	public static final String REGIS_END_TIME = "2018-08-20 23:59:59";
+	public static final String REGIS_END_TIME = "2018-08-21 15:30:00";
 }
